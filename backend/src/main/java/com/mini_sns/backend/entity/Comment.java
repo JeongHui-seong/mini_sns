8249412ccf2638a581c8a376282f99f20cffaaed
jungmin9911 <jmahn1819@gmail.com>
2025-07-23 12:36:01 +0900

    엔티티 작성

---
+++ backend/src/main/java/com/mini_sns/backend/entity/Comment.java
@@ -0,0 +1,40 @@
+package com.mini_sns.backend.entity;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long commentId;
+
+    @Column(length = 500)
+    private String content;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member writer;
+
+    @ManyToOne
+    @JoinColumn(name = "board_id")
+    private Board board;
+}
--- backend/src/main/java/com/mini_sns/backend/repository/MemberRepository.java
+++ backend/src/main/java/com/mini_sns/backend/repository/MemberRepository.java
@@ -2,8 +2,8 @@ package com.mini_sns.backend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.mini_sns.backend.entity.MemberEntity;
+import com.mini_sns.backend.entity.Member;
 
-public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long> {
   boolean existsByEmail(String email); // 이메일 중복체크
 }
