8ee4d17ef4cfa70d7b81c3e2ccb37a17d3357c9e
jungmin9911 <jmahn1819@gmail.com>
2025-07-23 21:39:25 +0900

    게시판 작업중

---
+++ backend/src/main/java/com/mini_sns/backend/repository/BoardRepository.java
@@ -0,0 +1,13 @@
+package com.mini_sns.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mini_sns.backend.entity.Board;
+
+@Repository
+public interface BoardRepository extends JpaRepository<Board, Long> {
+
+    
+
+}
